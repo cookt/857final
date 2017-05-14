6772106e0a255a7e952d7172e406f470b72eac71
Thomas Cook <tomcook@mit.edu>
2017-05-14 17:09:23 -0400

    should work for ropsten

--- decode_logs.js
+++ decode_logs.js
@@ -12,9 +12,9 @@ if (typeof web3 !== 'undefined') {
     logger.info("Coinbase: "+web3.eth.coinbase);
 }
 const SolidityCoder = require("web3/lib/solidity/coder.js");
-var API_URL = "http://api.etherscan.io/api?module=account&action=txlist";
+//var API_URL = "http://api.etherscan.io/api?module=account&action=txlist";//mainnet
 var API_TOKEN = "&apikey=CCT1MY4RTXUIYV6BXF5W1TKW4J3E71W3PG";
-
+var API_URL = "http://ropsten.etherscan.io/api?module=account&action=txlist";
 
 //Contract object to gather data and analzye
 function ContractHistory(name, address, abi) {
@@ -141,32 +141,36 @@ function AddressHistory(address) {
     this.txns = [];
     this.type = 0; // 0 for rando , other id for known entities
 }
-ethereum_lottery_address = "0x9473BC8BB575Ffc15CB2179cd9398Bdf5730BF55";
-ethereum_lottery_abi = require("./abis/TheEthereumLottery.json");
-var eth_lottery = new ContractHistory("EthereumLottery", ethereum_lottery_address, ethereum_lottery_abi);
+// ethereum_lottery_address = "0x9473BC8BB575Ffc15CB2179cd9398Bdf5730BF55";
+// ethereum_lottery_abi = require("./abis/TheEthereumLottery.json");
+// var eth_lottery = new ContractHistory("EthereumLottery", ethereum_lottery_address, ethereum_lottery_abi);
 
-eth_lottery.getTxns();
+// eth_lottery.getTxns();
 
-honest_dice_address = "0xD79B4C6791784184e2755B2fC1659eaaB0f80456";
-honest_dice_abi = require("./abis/HonestDice.json");
-var honest_dice = new ContractHistory("HonestDice", honest_dice_address, honest_dice_abi);
+// honest_dice_address = "0xD79B4C6791784184e2755B2fC1659eaaB0f80456";
+// honest_dice_abi = require("./abis/HonestDice.json");
+// var honest_dice = new ContractHistory("HonestDice", honest_dice_address, honest_dice_abi);
 
-honest_dice.getTxns();
+// honest_dice.getTxns();
 
-etheroll_address = "0x8F3d6447a647Ecf3c185ecbB165D2e6C41FAd547";
-etheroll_abi = require("./abis/Etherroll.json");
-var etheroll = new ContractHistory("Etheroll",etheroll_address, etheroll_abi);
+// etheroll_address = "0x8F3d6447a647Ecf3c185ecbB165D2e6C41FAd547";
+// etheroll_abi = require("./abis/Etherroll.json");
+// var etheroll = new ContractHistory("Etheroll",etheroll_address, etheroll_abi);
 
-etheroll.getTxns();
+// etheroll.getTxns();
 
-rouleth_35_address = "0x18a672E11D637fffADccc99B152F4895Da069601";
-rouleth_35_abi = require("./abis/Rouleth3.5.json");
-var rouleth_35 = new ContractHistory("Rouleth3.5",rouleth_35_address,rouleth_35_abi);
+// rouleth_35_address = "0x18a672E11D637fffADccc99B152F4895Da069601";
+// rouleth_35_abi = require("./abis/Rouleth3.5.json");
+// var rouleth_35 = new ContractHistory("Rouleth3.5",rouleth_35_address,rouleth_35_abi);
 
-rouleth_35.getTxns();
+// rouleth_35.getTxns();
 
-rouleth_48_address = "0x908c41461Cddefb9F7B4d90C03B66c1C52Ab6093";
-rouleth_48_abi = require("./abis/Rouleth4.8.json");
-var rouleth_48 = new ContractHistory("Rouleth4.8", rouleth_48_address, rouleth_48_abi);
+// rouleth_48_address = "0x908c41461Cddefb9F7B4d90C03B66c1C52Ab6093";
+// rouleth_48_abi = require("./abis/Rouleth4.8.json");
+// var rouleth_48 = new ContractHistory("Rouleth4.8", rouleth_48_address, rouleth_48_abi);
 
-rouleth_48.getTxns();
+// rouleth_48.getTxns();
+//On ropsten
+
+simple_dao_test_address = "0x6e41c6ab279b57cbc813fa60b66a64910834c5b826f72034ef30e92e6c8fa336";
+simple_dao_abi;
